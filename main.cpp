186a82afd402b0c4d004040e6573858037e5ff5f
tpate0562 <56415907+tpate0562@users.noreply.github.com>
2022-06-17 21:10:43 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+//End header inclusions
+//Start Global Variables
+int lowerBound, upperBound
+double sum, integral, coefficient
+int power
+//End Global Variables
+//Main Function
+int main(){
+    calculateintegral();
+}
+void calculateIntegral(){
+    return 0;
+}
