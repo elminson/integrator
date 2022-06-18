c92b4e572c7e0a8bf705fba8966ade2846ddc800
tpate0562 <56415907+tpate0562@users.noreply.github.com>
2022-06-17 22:31:54 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include <cmath>
 #include <iostream>
+using namespace std;
 // End header inclusions
 // Start Global Variables
-int lowerBound, upperBound 
-double sum, integral, coefficient 
-int power
+int lowerBound;
+int upperBound;
+double sum;
+double integral;
+double coefficient;
+int power;
+int option;
 // End Global Variables
+//Functions
+void calculateIntegral() {
+	/*List of options*/
+    cout << "Option 1: Constant" << endl;
+    cout << "Option 2: Linear Function" << endl;
+    cout << "Option 3: Exponential Function" << endl;
+    cout << "Option 4: Logarithmic Function" << endl;
+    cout << "Option 5: Trigonometric Function/Reciprocal" << endl;
+    cout << "Option 6: Inverse Trigonometric Function" << endl;
+    cin >> option;
+}
 // Main Function
 int main() {
 	cout << "Lower bound: "; cin >> lowerBound;
-    cout << "Upper bound: "; cin >> upperBounderBound;
-    calculateintegral();
+    cout << "Upper bound: "; cin >> upperBound;
+    calculateIntegral();
+    return 0;
 }
-void calculateIntegral() {
-	return 0;
-}
+
