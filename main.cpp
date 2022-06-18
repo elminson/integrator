ce5133858c5027ce19ff4fd864929c027c506f28
tpate0562 <56415907+tpate0562@users.noreply.github.com>
2022-06-17 21:23:50 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-#include <iostream>
 #include <cmath>
-//End header inclusions
-//Start Global Variables
-int lowerBound, upperBound
-double sum, integral, coefficient
+#include <iostream>
+// End header inclusions
+// Start Global Variables
+int lowerBound, upperBound 
+double sum, integral, coefficient 
 int power
-//End Global Variables
-//Main Function
-int main(){
+// End Global Variables
+// Main Function
+int main() {
+	cout << "Lower bound: "; cin >> lowerBound;
+    cout << "Upper bound: "; cin >> upperBounderBound;
     calculateintegral();
 }
-void calculateIntegral(){
-    return 0;
+void calculateIntegral() {
+	return 0;
 }
