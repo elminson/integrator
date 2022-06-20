7da17f1af1e8255e5d5698c573beda6fe9a54613
tpate0562 <56415907+tpate0562@users.noreply.github.com>
2022-06-19 22:50:21 -0700

    Update main.cpp
    Added preliminary support for option 3

--- main.cpp
+++ main.cpp
@@ -24,15 +24,26 @@ void calculateIntegral() {
     cout << "Option 5: Trigonometric Function/Reciprocal" << endl;
     cout << "Option 6: Inverse Trigonometric Function" << endl;
     cout << "Option 7: Finish" << endl;
+    cout << "After all others, absolute value functions will be added" << endl;
     cin >> option;
+    /*Function for selected option 1*/
     if (option == 1){
         cin >> input;
         cout << input * (upperBound - lowerBound) << endl;
     }
+    /*End funtion for 1 and start function for 2*/
     if (option == 2){
         cout << "Slope: ";
         cin >> input;
         cout << (((input * lowerBound) + (input * upperBound)) / 2) * (upperBound - lowerBound) << endl;
+    }
+    /*End function for 2 and start function for 3*/
+    if (option == 3){
+        /*Goal is to ask for accuracy amount, then depending on accuracy needs divide the (upperbound-lowerbound) into thousands of small sections and get a riemann summation. These sums can be dynamically adjusted since i can use the upperbound-lowerbound as the baseline*/
+        cin >> power;
+        cout << pow(lowerBound, power);
+
+        
     }
 }
 // Main Function
@@ -46,6 +57,7 @@ int main() {
         lowerBound = switch2;
     }
     calculateIntegral();
+    
     return 0;
 }
 
